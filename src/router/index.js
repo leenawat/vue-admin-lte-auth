83c376f762b530037e7b0a1349326d00898a04cf
leenawat <leenawat.pa@gmail.com>
2019-09-11 14:37:07 +0700

    add main layout

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+
+import Router from 'vue-router'
+import MainLayout from '@/views/MainLayout'
+import Home from '@/views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: MainLayout,
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: Home
+        },
+        {
+          path: '/about',
+          name: 'about',
+          // route level code-splitting
+          // this generates a separate chunk (about.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+        }
+      ]
+    }
+  ]
+})
---
+++ src/views/MainLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper">
+    <!-- <va-navibar></va-navibar>
+    <va-slider :slideMenuItems="slideMenuItems"></va-slider>
+    <va-content-wrap></va-content-wrap> -->
+    <router-view/>
+  </div>
+
+</template>
+<script>
+// import VANaviBar from '../components/NaviBar.vue'
+// import VASlider from '../components/Slider.vue'
+// import VAContentWrap from '../components/ContentWrap.vue'
+// import slideMenuItems from '../lib/slideMenuItems.js'
+export default {
+  data () {
+    return {
+      // slideMenuItems: slideMenuItems
+    }
+  },
+  // components: {
+  //   'va-navibar': VANaviBar,
+  //   'va-slider': VASlider,
+  //   'va-content-wrap': VAContentWrap
+  // }
+}
+</script>
+<style>
+</style>
